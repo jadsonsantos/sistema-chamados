87b8d8e27db188ca32bd26a2ad5626c73d83013b
Jadson Santos <jadsondossantos5@gmail.com>
2023-04-07 10:29:40 -0300

    comecando

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,3 +1,27 @@
-const SignIn = () => <p>SignIn</p>
+import './SignIn.scss'
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
+
+const SignIn = () => {
+  const [email, setEmail] = useState('')
+  console.log(setEmail)
+
+  return (
+    <section className="signin">
+      <form className="signin__form">
+        <input
+          type="text"
+          placeholder="seu email"
+          value={email}
+          className="signin__button"
+        />
+        <input type="submit" value="acessar" className="signin__button" />
+      </form>
+      <Link to="/register" className="signin__link">
+        Criar uma conta
+      </Link>
+    </section>
+  )
+}
 
 export default SignIn
