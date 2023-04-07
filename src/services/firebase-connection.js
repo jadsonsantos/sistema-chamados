6e4b0bf8e6ae7e92537ab46999eedc4e3076fc6d
Jadson Santos <jadsondossantos5@gmail.com>
2023-04-07 20:11:48 -0300

    firebase connection

---
+++ src/services/firebase-connection.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyAWMzgVmRw8yR8Xx_0zT7l7ng6XksDVaTM',
+  authDomain: 'tickets-635b5.firebaseapp.com',
+  projectId: 'tickets-635b5',
+  storageBucket: 'tickets-635b5.appspot.com',
+  messagingSenderId: '568196394805',
+  appId: '1:568196394805:web:f661b2728037f8e6a0267c',
+  measurementId: 'G-WKK76MX9ZB',
+}
