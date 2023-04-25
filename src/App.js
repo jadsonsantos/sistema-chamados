0e09b94c26e0c8587a8710586b2ee16b4a6af4d5
Jadson Santos <jadsondossantos5@gmail.com>
2023-04-25 20:50:17 -0300

    react-toastify

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import './App.scss'
 import { BrowserRouter } from 'react-router-dom'
 import RoutesApp from './routes'
 import AuthProvider from './contexts/auth'
+import 'react-toastify/dist/ReactToastify.css'
+import { ToastContainer } from 'react-toastify'
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <AuthProvider>
+          <ToastContainer autoClose={3000} />
           <RoutesApp />
         </AuthProvider>
       </BrowserRouter>
